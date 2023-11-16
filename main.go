87725a0c3e9f66343790fd4a515d2cdd3484f0ea
Gabriel Jeronimo <gabrieljeronimo-@hotmail.com>
2023-11-16 16:09:13 -0300

    Add support to concurrency

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"strings"
+	"sync"
 
 	"github.com/google/go-github/v56/github"
 	"github.com/joho/godotenv"
@@ -15,8 +16,15 @@ type RequestBody struct {
 	NewOwner string `json:"new_owner"`
 }
 
+var wg sync.WaitGroup
+
 func main() {
 	args := os.Args[1:]
+
+	if len(args) < 2 {
+		log.Println("Insufficient arguments. Usage: go run main.go <owner> <new_owner>")
+		return
+	}
 
 	owner := args[0]
 	newOwner := args[1]
@@ -29,6 +37,8 @@ func main() {
 
 	ownerPersonalToken := os.Getenv("OWNER_PERSONAL_TOKEN")
 
+	client := github.NewClient(nil).WithAuthToken(ownerPersonalToken)
+
 	data, err := os.ReadFile("repositories.txt")
 
 	if err != nil {
@@ -36,22 +46,28 @@ func main() {
 	}
 
 	repositoriesArray := strings.Split(string(data), "\n")
-	client := github.NewClient(nil).WithAuthToken(ownerPersonalToken)
 
 	for _, repoName := range repositoriesArray {
-		_, response, err := client.Repositories.Transfer(
-			context.Background(),
-			owner,
-			repoName,
-			github.TransferRequest{NewOwner: newOwner},
-		)
-
-		if response.StatusCode != 202 && response.StatusCode != 200 {
-			log.Printf("Failed to transfer %s repository: %v", repoName, err)
-			continue
-		}
-
-		fmt.Printf("Repository %s transfered successfuly\n", repoName)
+		go transferRepository(owner, repoName, newOwner, client)
+		wg.Add(1)
 	}
 
+	wg.Wait()
+}
+
+func transferRepository(owner string, repoName string, newOwner string, client *github.Client) {
+	defer wg.Done()
+	_, response, err := client.Repositories.Transfer(
+		context.Background(),
+		owner,
+		repoName,
+		github.TransferRequest{NewOwner: newOwner},
+	)
+
+	if response.StatusCode != 202 && response.StatusCode != 200 {
+		log.Printf("Failed to transfer %s repository: %v", repoName, err)
+		return
+	}
+
+	fmt.Printf("Repository %s transfered successfuly\n", repoName)
 }
